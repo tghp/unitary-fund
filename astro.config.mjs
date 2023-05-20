80b658fb916190cefe0e1814be35dab65c884c05
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2023-05-20 14:18:56 +0100

    Add GitHub pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,23 @@ import remarkDirective from 'remark-directive';
 import { imageDirective, gistDirective } from './src/remark/directives';
 import mdx from '@astrojs/mdx';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://unitaryfund.tghp.co.uk',
   integrations: [tailwind(), mdx(), react()],
   markdown: {
-    remarkPlugins: [[remarkOembed, {
-      syncWidget: true
-    }], remarkDirective, imageDirective, gistDirective]
-  }
+    remarkPlugins: [
+      [
+        remarkOembed,
+        {
+          syncWidget: true,
+        },
+      ],
+      remarkDirective,
+      imageDirective,
+      gistDirective,
+    ],
+  },
 });
