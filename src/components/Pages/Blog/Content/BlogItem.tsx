04e98f612f3cdd384396960b9864175b1aff6c64
Josh Davenport-Smith <josh.davenport@tghp.co.uk>
2023-06-26 11:32:17 +0100

    Corrected blog link

--- src/components/Pages/Blog/Content/BlogItem.tsx
+++ src/components/Pages/Blog/Content/BlogItem.tsx
@@ -10,8 +10,7 @@ export function BlogItem({ post: { data: post, slug } }: BlogItemProps) {
       <header className="flex  flex-col md:flex-row p-2 pl-3 mdpl-4 border-b  border-black">
         <time
           className="font-mono"
-          dateTime={`${post.publishYear}-${post.publishMonth}-${post.publishDay}`}
-        >
+          dateTime={`${post.publishYear}-${post.publishMonth}-${post.publishDay}`}>
           {post.publishDay}.{`${post.publishMonth}`}.{post.publishYear}
         </time>
         <div className="mt-1 md:mt-0 md:ml-3 font-bold">{post.author}</div>
@@ -22,9 +21,8 @@ export function BlogItem({ post: { data: post, slug } }: BlogItemProps) {
       <footer className=" mb-0">
         <a
           className="font-bold border-l-4 block pl-4 border-black text:lg md:text-xl"
-          href={`posts/${slug}`}
-          title={post.title}
-        >
+          href={`/posts/${slug}`}
+          title={post.title}>
           Read More
         </a>
       </footer>
